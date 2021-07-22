bfc60045fd5fab54ce86dccc908148fa83b707f1
akryilov <ka_dikk@mail.ru>
2021-07-22 18:39:16 +0300

    add homework for lesson2

---
+++ lesson2/geekshop/geekshop/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+
+def index(request):
+    context = {
+        'slogan': 'супер предложения',
+    }
+    return render(request, 'geekshop/index.html', context)
+
+
+def contacts(request):
+    return render(request, 'geekshop/contact.html')
